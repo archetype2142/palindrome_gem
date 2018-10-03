9fa5132f5b463017a1289814dd5e841a1b7fb528
Ritwick Malhotra <malhotraritwick2011@gmail.com>
2018-10-03 10:44:39 +0200

    final gem

--- lib/archetype2142_palindrome.rb
+++ lib/archetype2142_palindrome.rb
@@ -1,5 +1,12 @@
 require "archetype2142_palindrome/version"
 
-module Archetype2142Palindrome
-  # Your code goes here...
+class String
+	def palindrome?
+		processed_content == processed_content.reverse
+	end
+
+	private
+	def processed_content
+		self.scan(/[a-z]/i).join.downcase
+	end
 end
--- test/archetype2142_palindrome_test.rb
+++ test/archetype2142_palindrome_test.rb
@@ -5,7 +5,19 @@ class Archetype2142PalindromeTest < Minitest::Test
     refute_nil ::Archetype2142Palindrome::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_non_palindrome
+   	assert !"apple".palindrome?
+  end
+
+  def test_literal_palindrome
+  	assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+	assert "RaceCar".palindrome?	
+  end
+
+  def test_palindrome_with_punctuation
+    assert "ra.c; e C'aR".palindrome?
   end
 end
